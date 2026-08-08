f5480a5f8e3739cf8bec918eb365310d110fad65
mishabhi <mishabhi@gmail.com>
2026-08-08 00:18:40 -0700

    deploy 20260808_0018

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -27,5 +27,5 @@ const CONFIG = {
   API_BASE: '/api',
 
   // Stamped by deploy.sh at deploy time. 'dev' means running unstamped locally.
-  VERSION: '20260806_1941',
+  VERSION: '20260808_0018',
 };
